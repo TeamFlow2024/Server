8fff4865327d0371c941e8c3d2a8b815d62020cb
yewon <128056568+jeffyea320@users.noreply.github.com>
2025-02-12 15:58:53 +0900

    setting

---
+++ spring-boot-project-vs-code/src/main/java/com/teamflow/spring_boot_project_vs_code/SpringBootProjectVsCodeApplication.java
@@ -0,0 +1,13 @@
+package com.teamflow.spring_boot_project_vs_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectVsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectVsCodeApplication.class, args);
+	}
+
+}
---
+++ spring-boot-project-vs-code/src/test/java/com/teamflow/spring_boot_project_vs_code/SpringBootProjectVsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamflow.spring_boot_project_vs_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectVsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
