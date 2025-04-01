07e0a1a64cb91719434009d87f97f670b17ad18a
yewon <128056568+jeffyea320@users.noreply.github.com>
2025-04-01 16:11:38 +0900

    .

--- spring-boot-project-vs-code/src/main/java/com/teamflow/service/ProfileService.java
+++ spring-boot-project-vs-code/src/main/java/com/teamflow/service/ProfileService.java
@@ -14,14 +14,15 @@ public class ProfileService {
     private final UserRepository userRepository;
     private final S3Uploader s3Uploader;
 
-    public String saveProfileImage(String username, MultipartFile image) {
-        User user = userRepository.findByUsername(username)
+    public String saveProfileImage(String userId, MultipartFile image) {
+        User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
+    
         String imageUrl = s3Uploader.uploadFile(image, "profile");
         user.setProfile(imageUrl);
         userRepository.save(user);
-
+    
         return imageUrl;
     }
+    
 }
