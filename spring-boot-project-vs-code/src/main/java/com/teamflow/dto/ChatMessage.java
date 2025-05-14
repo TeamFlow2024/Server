ab72b671253e4d4e3145b5aba3dfb5ffa03fad19
yewon <128056568+jeffyea320@users.noreply.github.com>
2025-05-15 00:30:29 +0900

    변수 타입 수정

--- spring-boot-project-vs-code/src/main/java/com/teamflow/dto/ChatMessage.java
+++ spring-boot-project-vs-code/src/main/java/com/teamflow/dto/ChatMessage.java
@@ -5,8 +5,8 @@ import lombok.*;
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor @Builder
 public class ChatMessage {
-    private Long senderId;
-    private Long receiverId; // 개인채팅 시 수신자ID
+    private String senderId;
+    private String receiverId; // 개인채팅 시 수신자ID
     private Long teamId;     // 팀채팅 시 팀ID
     private String senderName;
     private String content;
